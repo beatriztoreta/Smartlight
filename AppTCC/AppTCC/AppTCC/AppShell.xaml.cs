3d06ea8dea3d91f6c4f21229a8aead1e9533c34a
Beatriz Toreta Pereira <beatriztoreta@hotmail.com>
2020-09-08 14:23:58 -0300

    Teste Telas
    Teste Telas

--- AppTCC/AppTCC/AppTCC/AppShell.xaml.cs
+++ AppTCC/AppTCC/AppTCC/AppShell.xaml.cs
@@ -11,6 +11,7 @@ namespace AppTCC
         public AppShell()
         {
             InitializeComponent();
+
             Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
             Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
