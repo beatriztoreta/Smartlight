a1b57f9da0e2e9b5ac023cea5b59f38d379e1639
Alikawk <31568485+Alikawk@users.noreply.github.com>
2020-10-17 16:23:37 -0300

    Add files via upload

--- AppTCC/AppTCC/AppTCC.Android/MainActivity.cs
+++ AppTCC/AppTCC/AppTCC.Android/MainActivity.cs
@@ -6,11 +6,10 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using br.com.smartlight.Droid;
 
 namespace AppTCC.Droid
 {
-    [Activity(Label = "AppTCC", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "AppTCC", Icon = "@drawable/slicon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
